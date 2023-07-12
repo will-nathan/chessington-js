722abc2387009e71fa2092930ada047c067d4b1c
Will Nathan <will.nathan@softwire.com>
2023-07-12 11:41:43 +0100

    added first move for pawns

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,26 +1,27 @@
 import Square from '../square';
 import Piece from './piece';
 import Player from '../player';
+import GameSettings from '../gameSettings';
 
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
 
+    isSpaceAvailable(board) {
+        return board.getPiece(square)===undefined
+    }
+
     getAvailableMoves(board) {
         let availableMoves = new Array(0);
         const currentSquare = board.findPiece(this);
-        const move = this.player === Player.WHITE ? 1 : -1;
-        /* let move;
-        if (this.player == Player.WHITE) {
-            move = 1
-        }
-        if (this.player == Player.BLACK) {
-            move = -1
-        } */
+        const isWhite  = this.player === Player.WHITE;
+        const move = isWhite? 1 : -1;
+        const isFirstMove = ((isWhite && currentSquare.row===1) || (!isWhite && currentSquare.row===GameSettings.BOARD_SIZE-2));
         let square = Square.at(currentSquare.row + move, currentSquare.col);
         if (board.getPiece(square)===undefined){availableMoves.push(square);}
-
+        square = Square.at(currentSquare.row + 2*move, currentSquare.col);
+        if (isFirstMove && board.getPiece(square)===undefined) {availableMoves.push(square);}
         return availableMoves
     }
 }
