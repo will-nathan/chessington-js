96b2975bd46135efba60671e347d8b44ac7aca25
Will Nathan <will.nathan@softwire.com>
2023-07-12 11:24:15 +0100

    pawns can move up/down one

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
+import Square from '../square';
 import Piece from './piece';
+import Player from '../player';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,19 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = new Array(0);
+        const currentSquare = board.findPiece(this);
+        const move = this.player === Player.WHITE ? 1 : -1;
+        /* let move;
+        if (this.player == Player.WHITE) {
+            move = 1
+        }
+        if (this.player == Player.BLACK) {
+            move = -1
+        } */
+        let square = Square.at(currentSquare.row + move, currentSquare.col);
+        if (board.getPiece(square)===undefined){availableMoves.push(square);}
+
+        return availableMoves
     }
 }
